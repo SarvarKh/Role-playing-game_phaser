939ce3b88bbe6777117fd95ae4da6211f12c440e
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-06-29 19:56:40 +0500

    Setup initial menu scenes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import 'phaser';
+import config from './Config/config';
+import GameScene from './Scenes/GameScene';
+import BootScene from './Scenes/BootScene';
+import PreloaderScene from './Scenes/PreloaderScene';
+import TitleScene from './Scenes/TitleScene';
+import OptionsScene from './Scenes/OptionsScene';
+import CreditsScene from './Scenes/CreditsScene';
 
-import { SimpleScene } from './scenes/simple-scene';
+class Game extends Phaser.Game {
+  constructor () {
+    super(config);
+    this.scene.add('Boot', BootScene);
+    this.scene.add('Preloader', PreloaderScene);
+    this.scene.add('Title', TitleScene);
+    this.scene.add('Options', OptionsScene);
+    this.scene.add('Credits', CreditsScene);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Game');
+  }
+}
 
-const gameConfig = {
-  width: 680,
-  height: 400,
-  scene: SimpleScene
-};
-
-new Phaser.Game(gameConfig);
+window.game = new Game();
