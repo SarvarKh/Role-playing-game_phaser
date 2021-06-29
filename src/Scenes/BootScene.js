4f96eb3f935662a1b899a8a3561ab682e20b16ba
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-06-29 20:04:46 +0500

    Show credits menu

--- src/Scenes/BootScene.js
+++ src/Scenes/BootScene.js
@@ -6,7 +6,7 @@ export default class BootScene extends Phaser.Scene {
   }
 
   preload () {
-    this.load.image('logo', 'assets/game_logo.png');
+    this.load.image('logo', 'assets/game_logo1.png');
   }
 
   create () {
--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -1,30 +1,28 @@
 import 'phaser';
+import config from '../Config/config';
 
 export default class CreditsScene extends Phaser.Scene {
   constructor () {
     super('Credits');
   }
 
-  preload () {
-  }
-
   create () {
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
     this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
-    
+
     Phaser.Display.Align.In.Center(
       this.creditsText,
       this.zone
     );
-    
+
     Phaser.Display.Align.In.Center(
       this.madeByText,
       this.zone
     );
-    
+
     this.madeByText.setY(1000);
-    
+
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
       y: -100,
@@ -35,7 +33,7 @@ export default class CreditsScene extends Phaser.Scene {
         this.destroy;
       }
     });
-    
+
     this.madeByTween = this.tweens.add({
       targets: this.madeByText,
       y: -300,
