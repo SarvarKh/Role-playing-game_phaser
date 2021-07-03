ba992feef46e91f40d31678dcfe46854ae8df6ab
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-03 21:25:59 +0500

    Add testing for getscore and highscore

--- src/helper/localstorage.test.js
+++ src/helper/localstorage.test.js
@@ -1,7 +1,11 @@
 /**
  * @jest-environment jsdom
  */
-import { setplayer } from './localstorage';
+import { 
+    setplayer,
+    highscore,
+    getscore
+ } from './localstorage';
 
 describe("Manupulating LocalStorage data", () => {
     beforeEach(() => {
@@ -12,7 +16,22 @@ describe("Manupulating LocalStorage data", () => {
             'player': "Romano"
         };
         expect(localStorage.getItem("player")).toEqual(dataAddedToLS.player);
+        expect(localStorage.getItem("player")).not.toEqual("Melisa");
         
         localStorage.clear();
     })
+
+    test('Set the score if the key-player is in localStorage', () => {
+        highscore(100);
+        expect(parseInt(localStorage.getItem('highscore'), 10)).toBe(100);
+    
+        expect(localStorage.getItem('highscore')).not.toBe(200);
+    });
+
+    test('Set the score if the key-player is in localStorage', () => {
+        getscore(100);
+        expect(parseInt(localStorage.getItem('score'), 10)).toBe(100);
+
+        expect(localStorage.getItem('score')).not.toBe('XYZ');
+    });
 })
